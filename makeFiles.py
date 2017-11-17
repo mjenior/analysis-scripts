1ffff689077edf4a173893af137911912721e14e
Matt Jenior <mljenior@umich.edu>
2017-11-17 14:09:20 -0500

    simplified makeFiles.py

--- makeFiles.py
+++ makeFiles.py
@@ -7,10 +7,7 @@ import glob
 fastqR1 = glob.glob('*R1*')
 
 with open(sys.argv[1], 'w') as outFiles:
-
 	for R1 in fastqR1:
-		name = str(R1).split('_')[0]
-		R2 = R1.replace('R1','R2')
-		entry = name + '\t' + R1 + '\t' + R2 + '\n'
+		entry = str(R1).split('_')[0] + '\t' + R1 + '\t' + R1.replace('R1','R2') + '\n'
 		outFiles.write(entry)
 
