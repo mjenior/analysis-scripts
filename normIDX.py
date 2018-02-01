d677f9b54e12c70ff6fdca318b85d71468fd907f
Matt Jenior <mljenior@umich.edu>
2018-02-01 09:28:44 -0500

    reworking normIDX

--- normIDX.py
+++ normIDX.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python
-# USAGE: normIDX.py input_file 250
+# USAGE: normIDX.py input_file ioutput_file 100
 # Normalizes read abundances to gene and read length
 
 import sys
 
 with open(sys.argv[1], 'r') as idxStats:
 
-        outFile = str(sys.argv[1]).rstrip('tcsvx') + 'norm.tsv'
-        outFile = open(outFile, 'w')
+        #outFile = str(sys.argv[1]).rstrip('cstvx') + 'norm.tsv'
+        #outFile = open(outFile, 'w')
+        outFile = open(ys.argv[2], 'w')
         outFile.write('contigName\tcontigLen\tnormDepth\n')
 
-        readLen = float(sys.argv[2])
+        #readLen = float(sys.argv[2])
+        readLen = float(sys.argv[3])
 
         for line in idxStats:
                 if line[0] == '*': continue
