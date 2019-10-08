b172b44f639817c64b7ab3c30d2ac3035b1a91e7
Matt Jenior <mattjenior@gmail.com>
2019-10-08 09:56:05 -0400

    totally reworked getSeqs with more intelligent arguments

--- getSeqs.py
+++ getSeqs.py
@@ -1,26 +1,49 @@
 #!/usr/bin/python
-'''USAGE: getSeqs.py BLAST_results fasta_file
+'''USAGE: getSeqs.py --blast BLAST_results --fasta fasta_file --out screened_fasta --include True
 Pulls out selected sequences from large fasta file based on tab-formatted blast results
 '''
 import sys
+import argparse
 
-bestHits = set()
-with open(sys.argv[1],'r') as blastOut:
+# User defined arguments
+parser = argparse.ArgumentParser(description='Screen Fasta files based on BLAST hits')
+parser.add_argument('--blast', default='default', help='Tab-formatted BLAST output file')
+parser.add_argument('--fasta', default='default', help='Fasta file to be screened')
+parser.add_argument('--out', default='default', help='Name of output fasta file')
+parser.add_argument('--include', default=True, help='Include or exclude those sequences found in BLAST file (True of False)')
 
-	for line in blastOut:
-		bestHits |= set([line.split()[0]])
+args = parser.parse_args()
+blast_file = str(args.blast)
+fasta_file = str(args.fasta)
+out_fasta = str(args.out)
+keep = args.include
 
-out_fasta = str(sys.argv[2]).rstrip('fastn') + 'filter.fasta'
+if blast_file == 'default' or fasta_file == 'default':
+	raise NameError('Required input files not provided')
+
+if out_fasta == 'default':
+	out_fasta = str(out_fasta).rstrip('fastn') + 'filter.fasta'
 out_fasta = open(out_fasta, 'w')
 
+bestHits = set()
+with open(blast_file, 'r') as blastOut:
+	for line in blastOut: bestHits |= set([line.split()[0]])
+
 include_seq = 0
-with open(sys.argv[2], 'r') as fasta:
+with open(fasta_file, 'r') as fasta:
 
 	for line in fasta:
 
 		if line[0] == '>':
 			name = line.strip().replace('>','')
-			if name in bestHits:
+
+			if keep != True:
+				if not name in bestHits:
+					out_fasta.write(line)
+					include_seq = 1
+					continue
+			else:
+				if not name in bestHits:
 				out_fasta.write(line)
 				include_seq = 1
 				continue
