535b2e01b41400faa1b14a88890b589308df9e01
Matt Jenior <mattjenior@gmail.com>
2020-11-24 13:23:17 -0500

    new output for pruneBins

--- pruneBins.py
+++ pruneBins.py
@@ -12,7 +12,9 @@ from shutil import copyfile
 parser = argparse.ArgumentParser(description='Prune contig bins based on quality data')
 parser.add_argument('outlier_table')
 parser.add_argument('--ext', default='fa', help='Bin fasta file extension')
-parser.add_argument('--bins', default='bins/', help='Directory for metagenomic contig bins')
+parser.add_argument('--bins', default='./', help='Directory for metagenomic contig bins')
+parser.add_argument('--report', default='report.txt', help='Output text file with pruning report')
+
 args = parser.parse_args()
 
 # Assign variables
@@ -41,51 +43,75 @@ with open(str(args.outlier_table), 'r') as outliers:
 
 
 # Read bin fasta with outliers and write them to new files, omitting outliers
-total_include = 0
-total_exclude = 0
-countsDict = {}
+total_include_contigs = 0
+total_exclude_contigs = 0
+total_include_bases = 0
+total_exclude_bases = 0
+newFiles = []
 print('\nOutlier contigs pruned:')
 for index in bin_list:
 	bin_name = index + fasta_ext
 	new_bin_name = index + '.pruned.fna'
 	new_bin = open(new_bin_name, 'w')
-	in_count = 0
-	out_count = 0
+	in_contigs = 0
+	out_contigs = 0
+	include_bases = 0
+	exclude_bases = 0
+
 	with open(bin_name, 'r') as fasta:
 
 		excluding = 0
 		exclude = outlier_dict[index]
 		for line in fasta:
 			if line[0] == '>':
-				in_count += 1
+				in_contigs += 1
 				seq_name = line.strip().replace('>','')
 				if not seq_name in exclude:
-					out_count += 1
+					out_contigs += 1
 					excluding = 0
-					total_include += 1
+					total_include_contigs += 1
 					new_bin.write(line)
 					continue
 				elif seq_name in exclude:
 					excluding = 1
-					total_exclude += 1
+					total_exclude_contigs += 1
 					continue
 			elif excluding == 0:
+				total_include_bases += len(line.strip())
+				include_bases += len(line.strip())
 				new_bin.write(line)
+				continue
+			else:
+				total_exclude_bases += len(line.strip())
+				exclude_bases += len(line.strip())
 
 	new_bin.close()
-	in_count = '(' + str(in_count) + ')'
-	out_count = '(' + str(out_count) + ')'
-	print(bin_name, in_count, '-->', new_bin_name, out_count)
+	outStr = bin_name + ' (' + str(in_contigs) + ' contigs) --> ' + new_bin_name + ' (' + str(out_contigs) + ' contigs)\n'
+	newFiles.append(outStr)
+
+
+# Open report doc
+reportFile = open(str(args.report), 'w')
 
 # Report a few stats to the user
-print('\nContigs excluded: ' + str(total_exclude))
-print('Contigs included: ' + str(total_include))
-perc_exclude = (float(total_exclude) / float(total_include)) * 100.0
-perc_exclude = round(perc_exclude, 3)
-print('Percent exclusion: ' + str(perc_exclude) + '%\n')
+reportFile.write('Contigs excluded: ' + str(total_exclude_contigs) + '\n')
+reportFile.write('Contigs included: ' + str(total_include_contigs) + '\n')
+perc_exclude_contigs = (float(total_exclude_contigs) / float(total_include_contigs)) * 100.0
+perc_exclude_contigs = round(perc_exclude_contigs, 3)
+reportFile.write('Percent contig exclusion: ' + str(perc_exclude_contigs) + '%\n')
+
+reportFile.write('\nBases excluded: ' + str(total_exclude_bases) + '\n')
+reportFile.write('Bases included: ' + str(total_include_bases) + '\n')
+perc_exclude_bases = (float(total_exclude_bases) / float(total_include_bases)) * 100.0
+perc_exclude_bases = round(perc_exclude_bases, 3)
+reportFile.write('Percent base exclusion: ' + str(perc_exclude_contigs) + '%\n\n')
+
+# Write files names and size changes to report
+reportFile.write('Outlier contigs pruned from:\n')
+for x in newFiles: reportFile.write(x)
 
 # Simply rename a copy of those fastas without outliers for continuity
-print('\nNo outlier contigs detected:')
+reportFile.write('\nNo outlier contigs detected in:\n')
 fastas = '*' + fasta_ext
 fastas = glob.glob(fastas)
 unchanged = []
@@ -96,8 +122,8 @@ for current in fastas:
 		src = current + fasta_ext
 		dst = current + '.pruned.fna'
 		copyfile(src, dst)
-		print(src, '-->', dst)
+		reportFile.write(src + ' --> ' + dst + '\n')
 
-print('')
+reportFile.close()
 if curr_wd != bin_dir:
 	os.chdir(curr_wd)
